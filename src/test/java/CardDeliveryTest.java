cc210ce4d2bc06d5991d3dc091c643fd22e38fd7
Burdakova Daria <d.saharova@inbox.ru>
2023-03-21 22:42:56 +0200

    hardcore date changed to generated date; --info added to yml

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -14,20 +14,21 @@ public class CardDeliveryTest {
     private String generateDate(int addDays, String pattern) {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
+
     @Test
     void shouldBookApointment() {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Москва");
         String currentDate;
-        currentDate = generateDate(4,"dd.MM.yyyy");
+        currentDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a");
         $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue("25.03.2023");
+        $("[data-test-id='date'] input").setValue(currentDate);
         $("[data-test-id='name'] input").setValue("Иван Иванов");
         $("[data-test-id='phone'] input").setValue("+79998887766");
         $("[data-test-id='agreement'] span").click();
         $(byText("Забронировать")).click();
         $("[data-test-id='notification']").should(Condition.appear, Duration.ofSeconds(15));
-
+        $("[data-test-id='notification']").shouldHave(Condition.text("Встреча успешно забронирована на " + currentDate);
     }
 }
